77373269e1ac310ffc6aba9e0bd116a8bb748005
Candela94 <candelafsg@alumnos.cei.es>
2025-02-10 19:02:48 +0100

    cambios

--- books-nest/src/pages/DetalleLibro.jsx
+++ books-nest/src/pages/DetalleLibro.jsx
@@ -1,29 +1,23 @@
+import { useEffect, useState } from "react";
 
 
 
 const DetalleLibro = () => {
 
 
-const libro = {
+    const[libro, setLibro] = useState("")
 
-    nombre:"Cien años de soledad",
-    autor:"Gabriel García Márquez",
-    genero: "Realismo mágico",
-    estado:"Como nuevo",
-    disponibilidad: true,
-    editorial: "",
-    
+useEffect(() => {
 
-    descripcion:"Cien años de soledad se centra en la familia Buendía de Riohacha y la maldición que llevan con ellos, por involucrarse entre parientes. Tal es el caso de José Arcadio Buendía y Úrsula Iguarán, quienes son primos y viven con el temor de engendrar un hijo con cola de cerdo. ",
-    estado:"Nuevo",
-    img:"https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.buscalibre.es%2Flibro-cien-anos-de-soledad%2F9789588886213%2Fp%2F46863379&psig=AOvVaw0OihD0D1GLr92xJtEH4GEe&ust=1739265578180000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCIiygq7juIsDFQAAAAAdAAAAABAE",
-
-
-}
+    fetch('public/backend/libros.json')
+    .then(response => response.json())
+    .then(data=> {
+        setLibro(data)
+    })
 
 
 
-const {nombre, autor, genero, estado, disponibilidad, descripcion, img} = libro;
+})
 
 
 return(
@@ -34,17 +28,17 @@ return(
         <img  src= {img} alt="Libro" className="Introduccion-imagen" />
 
         <div className="Introduccion-datos">
-            <h1 className="Nombre"> {nombre}</h1>
-            <p className="Decripcion">{descripcion}</p>
+            <h1 className="Nombre"> </h1>
+            <p className="Decripcion"></p>
         </div>
     </section>
 
 
     <section className="Detalles">
         <h2>Más detalles sobre el libro</h2>
-        <p>Autor : {autor}</p>
-        <p>Género: {genero}</p>
-        <p>Estado: {estado}</p>
+        <p>Autor :</p>
+        <p>Género: </p>
+        <p>Estado: </p>
 
     </section>
 
